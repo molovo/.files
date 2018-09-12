77d57855b1fe340b9cf343060e543f25eaaf8639
James Dinsdale <hi@molovo.co>
2018-09-12 17:00:42 +0100

    Sync valet config

---
+++ files/.config/valet/Drivers/JekyllValetDriver.php
@@ -0,0 +1,56 @@
+<?php
+class JekyllValetDriver extends ValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        if (is_dir($sitePath . '/_site')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        if (file_exists($staticFilePath = $sitePath . '/_site/' . $uri . '/index.html')) {
+            return $staticFilePath;
+        }
+        if (file_exists($staticFilePath = $sitePath . '/_site/' . $uri . 'index.html')) {
+            return $staticFilePath;
+        }
+        if (file_exists($staticFilePath = $sitePath . '/_site/' . $uri)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        return $sitePath . '/_site/index.html';
+    }
+}
